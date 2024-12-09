9ba85c7248d276a579d2d53a0ede4a390a698542
so-faya <sophiaekwusie70@gmail.com>
2024-12-09 14:28:37 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/vote_chose/', // Add this line to ensure correct paths for GitHub Pages deployment
 })
